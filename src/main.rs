262e1219d3ba8167f869e4b78f23b0b9e6a06788
soir20 <71418127+soir20@users.noreply.github.com>
2022-05-06 23:03:28 -0400

    Add option for frames per row

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@ struct FrameRev {
     #[clap(short = 'h', long = "height")]
     frame_height: u32,
 
+    #[clap(short = 'r', long = "frames-per-row")]
+    frames_per_row: Option<u32>,
+
     #[clap(parse(from_os_str), short = 'o', long = "output")]
     output: std::path::PathBuf,
 }
@@ -22,22 +25,29 @@ fn main() {
     let source = image::open(args.input)
         .expect("Image not found");
 
-    let frames_x = source.width() / args.frame_width;
-    let frames_y = source.height() / args.frame_height;
+    let src_frames_x = source.width() / args.frame_width;
+    let src_frames_y = source.height() / args.frame_height;
+
+    let total_frames = source.width() / args.frame_width * source.height() / args.frame_height;
+    let dest_frames_x = match args.frames_per_row {
+        Some(num) => num,
+        None => source.width() / args.frame_width
+    };
+    let dest_frames_y = div_ceil(total_frames, dest_frames_x);
 
     let mut dest = ImageBuffer::new(
-        frames_x * args.frame_width,
-        frames_y * args.frame_height
+        dest_frames_x * args.frame_width,
+        dest_frames_y * args.frame_height
     );
 
-    for x in 0..frames_x {
-        for y in 0..frames_y {
+    for x in 0..src_frames_x {
+        for y in 0..src_frames_y {
             let source_offset_x = x * args.frame_width;
             let source_offset_y = y * args.frame_height;
 
-            let frames_processed = x * frames_y + y;
-            let dest_offset_x = (frames_processed % frames_x) * args.frame_width;
-            let dest_offset_y = (frames_processed / frames_x) * args.frame_height;
+            let frames_processed = x * src_frames_y + y;
+            let dest_offset_x = (frames_processed % dest_frames_x) * args.frame_width;
+            let dest_offset_y = (frames_processed / dest_frames_x) * args.frame_height;
 
             let source_frame = source.view(
                 source_offset_x,
@@ -55,4 +65,8 @@ fn main() {
     }
 
     dest.save(args.output).expect("Unable to save image");
+}
+
+fn div_ceil(dividend: u32, divisor: u32) -> u32 {
+    dividend / divisor + ((dividend % divisor != 0) as u32)
 }
